fcd15925b135b030e779accf3d36a33b8d99ca68
Lucca Rauédys <luccarauedys@outlook.com>
2023-01-12 15:02:07 -0300

    exercicio04

--- exercicio04/src/App.java
+++ exercicio04/src/App.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String[] frutas = { "Morango", "Banana", "Uva", "Kiwi" };
+        String[] outrasFrutas = { "Morango", "Acerola", "Laranja", "Kiwi" };
+
+        ArrayList<String> itensEmComum = new ArrayList<>();
+
+        for (int i = 0; i < frutas.length; i++) {
+            if (Arrays.asList(outrasFrutas).contains(frutas[i]))
+                itensEmComum.add(frutas[i]);
+        }
+
+        System.out.println(itensEmComum);
     }
 }
